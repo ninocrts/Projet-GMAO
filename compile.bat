301761e0108d639f07651abec58d67186b86d3c8
OLUTCHI <furcone.olutchi@epfedu.fr>
2025-03-18 11:10:24 +0100

    init

---
+++ compile.bat
@@ -0,0 +1,2 @@
+%comspec% /c "C:\Program Files (x86)\Microsoft Visual Studio\2022\BuildTools\VC\Auxiliary\Build\vcvars64.bat"
+cl.exe /Zi /EHsc /Ivcpkg_installed/x64-windows/include /Fe:server.exe server.c /link vcpkg_installed/x64-windows/lib/websockets.lib 
